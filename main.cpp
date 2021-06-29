783a86c969475b5d372825a49b5500928ff28619
roodkcab <chenshuoshi@gmail.com>
2021-06-29 14:44:45 +0800

    test fill_preserve

--- main.cpp
+++ main.cpp
@@ -39,7 +39,6 @@ void CDrawingArea::draw_text(const Cairo::RefPtr<Cairo::Context>& cr, int width,
 	layout->get_pixel_size(text_width, text_height);
 	cr->move_to((width-text_width)/2, (height-text_height)/2);
 	layout->show_in_cairo_context(cr);
-	std::cout << font.get_size() << std::endl;
 }
 
 bool CDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
@@ -49,11 +48,19 @@ bool CDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 	int xc, yc;
 	xc = width / 2;
 	yc = height / 2;
-	cr->rectangle(10, 10, xc, yc);
+	cr->move_to(10, 10);
+	cr->line_to(200, 10);
+	cr->line_to(200, 100);
+	cr->line_to(10, 100);
+	cr->line_to(10, 10);
+	cr->set_source_rgba(0.8, 0, 0, 1);
+	cr->fill_preserve();
+	cr->set_line_width(10);
+	cr->set_source_rgba(0.8, 0.8, 0, 1);
 	cr->stroke();
+
 	cr->arc(xc, yc, width/2, -3.14, 3.14);
 	cr->stroke();
-	draw_text(cr, width, height);
 }
 
 int main(int argc, char *argv[])
