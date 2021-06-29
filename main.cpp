87dc86d8c4a2b07370aeaecd73aad0a1f28b1135
roodkcab <chenshuoshi@gmail.com>
2021-06-29 10:50:24 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <gtkmm.h>
 #include <gtkmm/drawingarea.h>
+#include <iostream>
 
 using namespace Gtk;
 
@@ -9,39 +10,50 @@ public:
 	CDrawingArea();
 	virtual ~CDrawingArea();
 protected:
-	bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height);
+	bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
+	void draw_text(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height);
 };
 
 CDrawingArea::CDrawingArea()
 {
-	signal_draw().connect(sigc::bind(sigc::mem_fun(*this, &CDrawingArea::on_draw), 300, 300));
+	signal_draw().connect(sigc::mem_fun(*this, &CDrawingArea::on_draw));
 }
 
 CDrawingArea::~CDrawingArea()
 {
+	std::cout << "clean" << std::endl;
 }
 
-bool CDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height)
+void CDrawingArea::draw_text(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height)
 {
+	Pango::FontDescription font;
+	font.set_family("Monospace");
+	//font.set_stretch(Pango::STRETCH_NORMAL);
+	font.set_size(30 * PANGO_SCALE);
+
+	auto layout = create_pango_layout("Hi there!");
+	layout->set_font_description(font);
+
+	int text_width;
+	int text_height;
+	layout->get_pixel_size(text_width, text_height);
+	cr->move_to((width-text_width)/2, (height-text_height)/2);
+	layout->show_in_cairo_context(cr);
+	std::cout << font.get_size() << std::endl;
+}
+
+bool CDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+	int width = this->get_width();
+	int height = this->get_height();
 	int xc, yc;
 	xc = width / 2;
 	yc = height / 2;
-
-	//cr->set_line_width(10.0);
 	cr->rectangle(10, 10, xc, yc);
 	cr->stroke();
-
 	cr->arc(xc, yc, width/2, -3.14, 3.14);
 	cr->stroke();
-
-	// draw red lines out from the center of the window
-	/*cr->set_source_rgb(0.8, 0.0, 0.0);
-	cr->move_to(0, 0);
-	cr->line_to(xc, yc);
-	cr->line_to(0, height);
-	cr->move_to(xc, yc);
-	cr->line_to(width, yc);
-	cr->stroke();*/
+	draw_text(cr, width, height);
 }
 
 int main(int argc, char *argv[])
