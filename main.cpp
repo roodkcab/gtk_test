f732b67b54145c4f702b4e4f6dff3165e33b223e
roodkcab <roodkcab@gmail.com>
2021-07-18 21:04:56 +0800

    set background

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 
 using namespace Gtk;
 
-static Cairo::RefPtr<Cairo::Surface> s;
 
 class CDrawingArea : public Gtk::DrawingArea
 {
 public:
 	CDrawingArea();
 	virtual ~CDrawingArea();
+	Cairo::RefPtr<Cairo::Surface> s;
 
 protected:
 	bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
+	bool on_event(GdkEvent *gdk_event);
 	bool on_configure(GdkEventConfigure* event);
 	bool offscreen_render();
 	void draw_text(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height);
@@ -23,6 +24,22 @@ CDrawingArea::CDrawingArea()
 {
 	signal_draw().connect(sigc::mem_fun(*this, &CDrawingArea::on_draw));
 	signal_configure_event().connect(sigc::mem_fun(*this, &CDrawingArea::on_configure));
+	add_events( static_cast<Gdk::EventMask>(Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK | Gdk::DOUBLE_BUTTON_PRESS | Gdk::BUTTON_MOTION_MASK));
+}
+
+bool CDrawingArea::on_event(GdkEvent *gdk_event)
+{
+	switch (gdk_event->type)
+	{
+		case GDK_BUTTON_PRESS:
+		case GDK_BUTTON_RELEASE:
+		case GDK_DOUBLE_BUTTON_PRESS:
+		case GDK_MOTION_NOTIFY:
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 CDrawingArea::~CDrawingArea()
@@ -32,19 +49,31 @@ CDrawingArea::~CDrawingArea()
 
 void CDrawingArea::draw_text(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height)
 {
+	Cairo::RefPtr<Cairo::LinearGradient> linearGradient = Cairo::LinearGradient::create(0, 0, 400, 0);
+	linearGradient->add_color_stop_rgba(0.0, 1.00, 0.00, 0.00, 1.0);
+	linearGradient->add_color_stop_rgba(1.0, 0.00, 1.00, 0.00, 1.0);
+	Cairo::Matrix m = cr->get_matrix();
+	m.rotate(3 * M_PI / 2);
+	m.translate(400, 0);
+	linearGradient->set_matrix(m);
+	cr->set_source(linearGradient);
+	cr->fill();
+
 	Pango::FontDescription font;
 	font.set_family("Monospace");
 	//font.set_stretch(Pango::STRETCH_NORMAL);
 	font.set_size(30 * PANGO_SCALE);
 
-	auto layout = create_pango_layout("测试一下中文怎么样测试一下中文怎么样测试一下中文怎么样测试一下中文怎么样");
+	auto layout = Pango::Layout::create(cr);
+	//auto layout = create_pango_layout();
+	layout->set_text("测试一下中文怎么样测试一下中文怎么样测试一下中文怎么样测试一下中文怎么样");
 	layout->set_font_description(font);
 	layout->set_width(static_cast<int>(width * Pango::SCALE));
 
-	int text_width;
-	int text_height;
+	int text_width, text_height;
 	layout->get_pixel_size(text_width, text_height);
 	cr->move_to((width-text_width)/2, (height-text_height)/2);
+	
 	layout->show_in_cairo_context(cr);
 }
 
@@ -64,17 +93,12 @@ bool CDrawingArea::offscreen_render()
 {
 	if (s && s->get_status() == CAIRO_STATUS_SUCCESS) {
 		Cairo::RefPtr<Cairo::Context> cr = Cairo::Context::create(s);
-		Cairo::RefPtr<Cairo::LinearGradient> linearGradient = Cairo::LinearGradient::create(0, 0, 400, 0);
-		linearGradient->add_color_stop_rgba(0.0, 1.00, 0.00, 0.00, 1.0);
-		linearGradient->add_color_stop_rgba(1.0, 0.00, 1.00, 0.00, 1.0);
-		Cairo::Matrix m = cr->get_matrix();
-		m.rotate(3 * M_PI / 2);
-		m.translate(400, 0);
-		linearGradient->set_matrix(m);
-		cr->set_source(linearGradient);
-		cr->rectangle(0, 0, 0, 0);
-		draw_text(cr, this->get_width(), this->get_height());
+		cr->set_source_rgba(1.f, 1.f, 1.f, 1.f);
+		cr->rectangle(0, 0, this->get_width(), this->get_height());
 		cr->fill();
+		
+		draw_text(cr, this->get_width(), this->get_height());
+		//cr->fill();
 		this->queue_draw();
 	}
 	return true;
@@ -85,6 +109,7 @@ int main(int argc, char *argv[])
 	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 	Gtk::Window window;
 	window.set_default_size(400, 400);
+	window.show();
 
 	CDrawingArea area;
 	window.add(area);
